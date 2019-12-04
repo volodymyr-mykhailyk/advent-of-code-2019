809b2282085536d8577374db28917f837e2220de
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2019-12-04 09:37:45 +0200

    Password verifier

---
+++ lib/universe/fuel_station/password_verifier.rb
@@ -0,0 +1,35 @@
+module Universe
+  module FuelStation
+    class PasswordVerifier
+      def valid?(password)
+        digits = convert_to_digits(password)
+
+        return false unless valid_length?(digits)
+        return false unless increasing_numbers?(digits)
+        return false unless repeated_numbers?(digits)
+
+        true
+      end
+
+      protected
+
+      def convert_to_digits(password)
+        password.to_s.split('').map(&:to_i)
+      end
+
+      def increasing_numbers?(digits)
+        digits.slice(0, 5).each_with_index.all? do |digit, index|
+          digit <= digits[index+1]
+        end
+      end
+
+      def repeated_numbers?(digits)
+        digits.uniq.count < 6
+      end
+
+      def valid_length?(digits)
+        digits.length == 6
+      end
+    end
+  end
+end
---
+++ spec/lib/fuel_station/password_verifier_spec.rb
@@ -0,0 +1,20 @@
+require 'spec_helper'
+require 'universe/fuel_station/password_verifier'
+
+RSpec.describe Universe::FuelStation::PasswordVerifier do
+  describe '.valid?' do
+    VERIFICATIONS = [
+      {input: 111111, output: true},
+      {input: 223450, output: false},
+      {input: 123789, output: false},
+      {input: 11111, output: false},
+      {input: 1111111, output: false},
+    ]
+
+    VERIFICATIONS.each do |input:, output:|
+      it "verifies #{input} password" do
+        expect(subject.valid?(input)).to eq(output)
+      end
+    end
+  end
+end
