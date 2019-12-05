c43725c668dab3a62811664d4fed819ec4b08b4f
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2019-12-05 20:23:02 +0200

    Extend runner for part 2 of day 5

--- lib/tasks/day05/runner.rb
+++ lib/tasks/day05/runner.rb
@@ -9,9 +9,14 @@ input_reader = Utils::InputReader.new(File.expand_path('input.txt'))
 program = input_reader.one_line.split_with(',').to_integer.read
 info "Computing program with #{program.length} operators"
 
-program_inputs = [1]
-computer = Universe::Ship::IntcodeComputer.new(program, program_inputs)
-
+info "Getting diagnostic code for air conditioner"
+computer = Universe::Ship::IntcodeComputer.new(program, [1])
 computer.run_program
 output = computer.output
-info "Program finished. Output #{output}"
+info "Program finished. Diagnostic code: #{output}"
+
+info "Getting diagnostic code for thermal radiator controller"
+computer = Universe::Ship::IntcodeComputer.new(program, [5])
+computer.run_program
+output = computer.output
+info "Program finished. Diagnostic code: #{output}"
