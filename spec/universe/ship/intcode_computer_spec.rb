c6943ae7fa475d445a855be7d56b61a5ac886857
Volodymyr Mykhailyk <712680+volodymyr-mykhailyk@users.noreply.github.com>
2019-12-05 13:32:49 +0200

    Add new instruction tests

--- spec/universe/ship/intcode_computer_spec.rb
+++ spec/universe/ship/intcode_computer_spec.rb
@@ -11,12 +11,19 @@ RSpec.describe Universe::Ship::IntcodeComputer do
       {program: [1, 1, 1, 4, 99, 5, 6, 0, 99], input: [], output: [], state: [30, 1, 1, 4, 2, 5, 6, 0, 99]},
       {program: [4, 0, 99], input: [], output: [4], state: [4, 0, 99]},
       {program: [1101, 100, -1, 4, 0], input: [], output: [], state: [1101, 100, -1, 4, 99]},
+      {program: [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], input: [8], output: [1], state: [3, 9, 8, 9, 10, 9, 4, 9, 99, 1, 8]},
+      {program: [3, 9, 8, 9, 10, 9, 4, 9, 99, -1, 8], input: [9], output: [0], state: [3, 9, 8, 9, 10, 9, 4, 9, 99, 0, 8]},
+      {program: [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], input: [7], output: [1], state: [3, 9, 8, 9, 10, 9, 4, 9, 99, 1, 8]},
+      {program: [3, 9, 7, 9, 10, 9, 4, 9, 99, -1, 8], input: [8], output: [0], state: [3, 9, 8, 9, 10, 9, 4, 9, 99, 0, 8]},
+      {program: [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], input: [5], output: [999], state: []},
+      {program: [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], input: [8], output: [1000], state: []},
+      {program: [3, 21, 1008, 21, 8, 20, 1005, 20, 22, 107, 8, 21, 20, 1006, 20, 31, 1106, 0, 36, 98, 0, 0, 1002, 21, 125, 20, 4, 20, 1105, 1, 46, 104, 999, 1105, 1, 46, 1101, 1000, 1, 20, 4, 20, 1105, 1, 46, 98, 99], input: [12], output: [1001], state: []},
     ]
 
     PROGRAMS.each do |program:, input:, output:, state:|
       it "produce correct state for #{program} program" do
         computer = described_class.new(program, input)
-        expect(computer.run_program).to eq(state)
+        expect(computer.run_program).to match(state)
       end
 
       it "produce correct output for #{program} program" do
